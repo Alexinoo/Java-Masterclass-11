ad1553ff2794ccde85fe837e29c8251b356d09a1
Alex Mwangi <alex@stl-horizon.com>
2024-06-11 18:00:13 +0300

    Hello World project

---
+++ src/first_steps/part1_hello_world/Hello.java
@@ -0,0 +1,63 @@
+package first_steps.part1_hello_world;
+
+/*
+ * Hello World Project
+ * ...................
+ * - Java programs (and most other programming languages) have keywords
+ * - Each has a specific meaning and sometimes they need to be used in specific orders
+ *
+ * - You write Java programs by following a specific set of rules, using a combination of these keywords and other
+ *   things you'll see which will collectively form a Java program
+ *
+ * N/B
+ * - Keywords are case-sensitive
+ *    - public and Public and even PUBLIC are different things
+ *
+ * - Below
+ *    - public and class are two java keywords
+ *    - they have a specific meaning which we'll find out more later in the course
+ *
+ * - Also notes that intelliJ highlights or bolds them
+ *
+ * public
+ *  - This is a Java keyword that is known as an access modifier
+ *  - An access modifier allows us to define the scope or how other parts of your code or even someone else's code can
+ *    access this code
+ *  - We use the public access modifier to give full access
+ *
+ * class
+ *  - The class keyword is used to define a class with the name following the keyword
+ *  - "Hello" in this case
+ *
+ * {}
+ *  - the left and right curly braces defines the class block
+ *
+ * Summary
+ * .......
+ *  - To define a class, it requires an optional access modifier, followed by class, then the class name , with opening left
+ *    and right curly braces
+ *  - The left and right curly braces defines the class body
+ *    - anything between them is "part" of this class
+ *  - we can have data and code inside the class body
+ *
+ *  - so that's our first project and our first class defined
+ *
+ *
+ *
+ */
+
+public class Hello {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
