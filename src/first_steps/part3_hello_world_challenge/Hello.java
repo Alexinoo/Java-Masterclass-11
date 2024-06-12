78e3f6265b4b833312a1cee78d83601c7ef9bc87
Alex Mwangi <alex@stl-horizon.com>
2024-06-12 06:14:34 +0300

    Hello World Challenge and common errors

---
+++ src/first_steps/part3_hello_world_challenge/Hello.java
@@ -0,0 +1,19 @@
+package first_steps.part3_hello_world_challenge;
+
+/*
+ * Hello World Challenge
+ * .....................
+ * How would you go about printing your name, instead of World
+ * Looking at this code see if you can figure that out, and get that to run
+ *
+ * Possible/Common Errors
+ *  - Missing semicolon
+ *  - Missing double quotes wrapping the string
+ *      - The text in the double quotes, is called a literal string
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, Alex");
+    }
+}
