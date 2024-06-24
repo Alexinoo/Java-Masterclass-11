50617acdb1cede5ec7232e9a7b50826dee1c66fc
Alex Mwangi <alex@stl-horizon.com>
2024-06-24 06:28:51 +0300

    Control FLow Statements - Introduction

---
+++ src/control_flow_statements/part1_introduction/Main.java
@@ -0,0 +1,24 @@
+package control_flow_statements.part1_introduction;
+/*
+ * Control FLow Statements - Introduction
+ * ......................................
+ *
+ * - We'll deal with control flow statements
+ *
+ * - This is a section in programming where we'll be able to control the flow of the program
+ *  - that is determining where the flow of the program is going to go and what code is going to be executed
+ *
+ * - We'll look at the
+ *      - switch statement
+ *      - for statement
+ *      - while statement
+ *      - do-while statement
+ *
+ * - These are the 4 key control flow statements that we need to understand to get the most out of Java
+ *
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
