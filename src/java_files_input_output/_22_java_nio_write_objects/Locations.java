91c006a46cde09ff23e33be509bf98e82404763f
Alex Mwangi <alex@stl-horizon.com>
2024-09-07 11:19:48 +0300

    Java.nio.file - Read and write objects with Files.newOutputStream() and Files.newInputStream()

--- src/java_files_input_output/_22_java_nio_write_objects/Locations.java
+++ src/java_files_input_output/_22_java_nio_write_objects/Locations.java
@@ -11,8 +11,104 @@ import java.util.Set;
 /*
  * Java NIO - Writing Objects with Java NIO
  *
- *  Modify static initializer so that it uses Files.newInputStream to read "locations_nio_objects_2.dat"
+ *
+ * /////////
+ * - When you use Files.newBufferedReader() or Files.newBufferedWriter() , to create the BufferedReader and
+ *   BufferedWriter objects, the underlying instance used to do the InputOutput is a java.nio instance rather than
+ *   java.io instance
+ *
+ *
+ * ///////////////
+ *
+ * - What about reading and writing objects ?
+ *
+ *  - There isn't a Files.newObjectInputStream or Files.newObjectOutputStream(), but there are Files.newInputStream and
+ *     Files.newOutputStream methods which return an InputStream and an OutputStream
+ *  - In short, you can construct an ObjectInputStream from an InputStream and an ObjectOutputStream from an
+ *    OutputStream
+ *  - We should be able to create an ObjectInputStream or an OutputStream from a path using a 2-step process
+ *  - So we call the Files method to get the appropriate stream and then call the ObjectInputStream or
+ *    ObjectOutputStream constructed directly using the stream you got from the Files method
+ *
+ * ///////
+ * - work with locations_nio_objects.dat files
+ *
+ * - Read from locations_nio_objects.dat and write to locations_nio_objects_2
+ *
+ * Locations.main()
+ * - Generating "locations_nio_objects_2.dat"
+ *  - Create a Path obj
+ *      Path locPath = FileSystems.getDefault().getPath("locations_nio_objects_2.dat");
+ *
+ *  - Create an ObjectOutputStream obj
+ *      - Pass new BufferedOutputStream obj to ObjectOutputStream constructor
+ *      - Pass Files.newOutputStream(locPath) to BufferedOutputStream constructor
+ *
+ *  - Loop through the values from the  Map<Integer, Location> locations : HashMap
+ *      - call writeObject() from ObjectOutputStream instance and pass individual locations
+ *
+ * /////// Few Things to note :
+ * - We're using Files.newOutputStream(locPath) to get an OutputStream using a Path instance by passing locPath which
+ *   is using java.nio.file.Path instance
+ * - Then secondly, we're doing something similar to what we did when we created FileOutputStream using the
+ *    FileOutputStream constructor
+ * - We're still wrapping it up in this BufferedOutputStream and so the IO will be buffered and then wrapping that up
+ *    in a ObjectOutputStream
+ * - The only change we've made to this code is the call to Files.newOutputStream(locPath) rather than the call to
+ *   FileOutputStream constructor
+ * - And so that's where the java.nio code comes in
+ *
+ *
+ *
+ * ////////////////
+ * - Modify static initializer so that it uses Files.newInputStream to read "locations_nio_objects_2.dat" created
+ *   from the Locations.main()
+ *
+ * - Create a Path obj/instance that points to the file generated after running Locations.main()
+ * - Use try-with-resources
+ *      - Create ObjectInputStream obj by passing a new BufferedInputStream object to ObjectInputStream constructor
+ *      - Pass Files.newInputStream(locPath) to BufferedInputStream constructor
+ *      - Initialize an eof boolean variable and set it to false
+ *      - Loop as long as eof is not reached
+ *          - Nest a try block
+ *              - call readObj() on locFile instance and cast it to Location obj
+ *              - add to Map<Integer, Location> locations : HashMap
+ *
+ *          - Handle EOFException by declaring a boolean eof variable and initialize it to false
+ *              - Set eof to true from the catch block as it will always be thrown after the readObject() on ObjectInputStream obj
+ *                  is done
+ *      - Catch
+ *          - InvalidClassException
+ *          - IOException
+ *          - ClassNotFoundException
+ *      - In that order and print out the message
+ *
+ * /////
+ * - The code is very similar or identical to what we used previously to read objects from locations.dat except
+ *    that we're using the Files.newInputStream() using that method to create an InputStream
+ * - Then we're wrapping that with a BufferedInputStream and finally wrapping that with an ObjectInputStream
+ *
+ *
+ * ////
+ * - So essentially we can use Files.newOutputStream and Files.newInputStream to create any stream that descends
+ *   from input stream and output stream respectively
+ *
+ * //////
+ *
  *  - Run Main.main() and ensure that we can still play the game
+ *
+ *
+ * ////////
+ * - At this point, we now know how to use java.nio classes to create commonly used java.io streams
+ * - We'll start looking at how to read and write to text files using java.nio exclusively
+ *
+ * - Remember that doing IO using java.nio, means the data is processed in blocks rather than 1 byte
+ *   or character at a time
+ * - To accomplish this java.nio uses channels and buffers and sometimes selectors
+ *
+ * - Let's start looking at channels, buffers and selectors and use java.nio to do our InputOuput
+ *
+ *
  */
 
 public class Locations implements Map<Integer, Location> {
