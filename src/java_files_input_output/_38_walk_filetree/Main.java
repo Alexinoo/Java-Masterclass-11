d49127ea1e84a2bac1569a0051f306f24b96c9c6
Alex <alex@stl-horizon.com>
2024-09-10 11:36:38 +0300

    java.nio.file.Files - walkFileTree , FileVisitor interface methods

--- src/java_files_input_output/_38_walk_filetree/Main.java
+++ src/java_files_input_output/_38_walk_filetree/Main.java
@@ -7,98 +7,134 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 
 /*
- * Walk File Tree
+ * Files.walkFileTree(Path path ,new PrintNames<Path>())
  *
- * - Means visiting every file, or directory that can be reached from a starting or root directory for that tree
+ * - Walking a FIle Tree in this case means visiting every file, or directory that can be reached from a starting or root directory for
+ *   that tree
  *
- * - For example, if we started in dir2 , we would visit the following but not in any particular order
+ * - For example, if we started in FileTree\dir2 in our project, we would visit the following files and directories but not necessarily in
+ *   any particular order
  *      - file1.txt , file2.txt and file3.dat
  *      - dir3
  *          - file1.txt
  *          - file2.txt
- * - dir2 will be considered as the root folder of its file tree , and we're never going to move up a level from the root directory, and we're only
- *   be moving down levels
+ * - Since we're going to sat at dir2 folder , this will be considered as the root folder of its file tree , and we're never going to move up
+ *   a level from the root directory, and we're only be moving down levels
  *
- * - But why do we have to do this ?
- *      - One use case might be when the application has to find a file, or the app is searching all the files in the directory and below for a
- *         specific string
+ *
+ * ///////
+ *
+ * - You might be wondering why would you want to do this ?
+ *
+ * - One use case might be when the application has to find a file,
+ * - Another use case could be when the application is searching all the files in the directory and below for a specific string
  * - When you want to copy a directory, you have to walk file tree if you want to copy everything below it as well
- *      - Otherwise, only the files directly within the directory itself would be copied
+ * - Otherwise, only the files directly within the directory itself would be copied
  * - As we saw earlier when we were using the DirectoryStream to get contents of a directory, only the direct descendants are returned by default
  *
  *
- * ///////////////////////
- * FileVisitor interface
- * //////////////////////
+ * - Let's now talk briefly about the FileVisitor interface, which is what we need to use to walk the directory tree
  *
- * Overview
+ * ///////////////////////////////
+ * FileVisitor interface Overview
+ * ///////////////////////////////
  *
- *  - To walk the directory tree, you'll have to use the FileVisitor Interface
- *  - Using the methods in FileVisitor, you can run code at each stage of the traversal process
- *  - Below are the methods you'll have to implement:
+ * - To walk the directory tree, you'll have to use the FileVisitor Interface
+ * - Using the methods in FileVisitor, you can run code at each stage of the traversal process
+ * - Below are the methods you'll have to implement:
  *
- *      - preVisitDirectory()
+ *      - preVisitDirectory( Path dir, BasicFileAttributes attrs)
  *          - this method accepts a reference to the directory, and the BasicFileAttributes instance for the directory
  *          - It's called before entries in the directory are visited
  *
- *      - postVisitDirectory()
- *          - this method accepts a reference to the directory and an exception object (when necessary)
+ *      - postVisitDirectory(Path dir, BasicFileAttributes attrs)
+ *          - this method accepts a reference to the directory and also an exception object (when necessary)
  *          - It's called after entries in the directory, and all it's descendants , have been visited
  *          - The exception parameter will be set when an exception happens during the traversal of the entries and descendants
- *
- *      - There's a way to skip files as you're traversing . so not every file has to have been visited for this method to be called.
+ *          ///////
+ *          - There's a way to skip files as you're traversing and so not every file has to have been visited for this method to be called.
  *          - Every file has to be visited or explicitly skipped
  *          - Of course, if an exception is thrown and not handled , the traversal will prematurely end and postVisitDirectory() will be called
  *
- *      - visitFile()
+ *      - visitFile(Path file, BasicFileAttributes attrs )
  *          - probably the method that you'll care most about
  *          - This method accepts a reference to the file and a BasicFileAttributes instance
  *          - This is where you run the code that will operate on the file
  *          - It's only called for files
  *
- *      - visitFileFailed()
+ *      - visitFileFailed(Path file, IOException exc)
  *          - this method is called when a file can't be accessed
  *          - The exception that's thrown is passed to this method
  *          - You can then decide what to do with it (throw it , print it, or anything else that makes sense for the application and operation
  *             being performed)
- *          - can be called for files and directories
+ *          - can be called for both files and directories
  *
  * - As with most interfaces like this , the jdk has got an implementation that you can use called SimpleFileVisitor
- *      - You can extend this class and override just the methods that you require
+ * - You can extend this class and override just the methods that you require
  *
  *
  * /////////
  * - Let's create a class that extends SimpleFileVisitor and traverse the file tree with dir2 as its root
- *      - We'll print the name of every file and directory it visits
+ * - We'll print the name of every file and directory it visits
  *
+ *      - Create PrintNames class -->
  *
+ * /////////////////\\\\\\\\\\\\\\\\
  * /////////////////\\\\\\\\\\\\\\\\\
  *
- * - Working with : Files.walkFileTree(Path start , FileVisitor<? super Path> visitor )
+ *  Files.walkFileTree(Path start , FileVisitor<? super Path> visitor )
  *
- *  - Start the traversal by using Files.walkFileTree(Path start , FileVisitor<? super Path> visitor )
+ * - Create a Path obj for the root directory where we're starting from
  *
- * - We have another version of walkFileTree() that accepts 4 parameters
- *      - The additional 2 parameters allow you to specify the number of levels to traverse and to pass a set of file visit options
- *      - Right now the only option available is whether to follow symbolic links
+ *      Path dir2Path = FileSystems.getDefault().getPath("FileTree" + File.separator +"Dir2");
+ *
+ * - Start the traversal by using Files.walkFileTree(Path start , FileVisitor<? super Path> visitor )
+ *
+ *      Files.walkFileTree(dir2Path , new PrintNames());
+ *
+ *      - pass the Path obj as the 1st parameter
+ *      - pass an instance of the PrintNames class that extends SimpleFileVisitor as the 2nd parameter
+ *
+ * - Catch any IOException
+ *
+ * - IF we run this :
+ *
+ *      --- Walking Tree for Dir2 ----
+ *
+        pre-visit : C:\JMC17\Java-Masterclass-11\FileTree\Dir2
+        pre-visit : C:\JMC17\Java-Masterclass-11\FileTree\Dir2\Dir3
+
+        visiting... C:\JMC17\Java-Masterclass-11\FileTree\Dir2\Dir3\file1.txt
+        visiting... C:\JMC17\Java-Masterclass-11\FileTree\Dir2\Dir3\file2.txt
+        visiting... C:\JMC17\Java-Masterclass-11\FileTree\Dir2\file1.txt
+        visiting... C:\JMC17\Java-Masterclass-11\FileTree\Dir2\file2.txt
+        visiting... C:\JMC17\Java-Masterclass-11\FileTree\Dir2\file3.dat
+ *
+ *      - and we can see that we're able to traverse to every node
+ *
+ * //////
+ *
+ * - There's also another version of walkFileTree() that accepts 4 parameters
+ *
+ * - The additional 2 parameters allow you to specify the number of levels to traverse and to pass a set of file visit options
+ *
+ * - Right now the only option available is whether to follow symbolic links
  *
  * - When we're printing out the names, it doesn't matter whether we use preVisitDirectory() to print directory names or postVisitDirectory()
  * - But depending on what you want to do, it can matter though
  *
  * - For example :
  *
- *  - if you wanted to copy a file tree, then you'd want to handle the directory before you handle it's entries because the copy
+ * - if you wanted to copy a file tree, then you'd want to handle the directory before you handle it's entries because the copy
  *   of the directory will have to exist before you can copy entries into it
- *      - In that scenario, you would actually handle the directory using the preVisitDirectory()
+ * - In that scenario, you would actually handle the directory using the preVisitDirectory()
  *
  *  - if you wanted to traverse a file tree to delete it, then you actually want to handle the directory in the postVisitDirectory() because you have
  *    to delete all the entries in the directory and all it's descendants before you can delete the directory.
- *  -
  *
- * /////
- *  - So, whether to handle directories pre or post will actually depend on makes sense for the type of processing that you want to do
- *  - You also can't make any assumptions about the order in which files and directories are visited, and shouldn't make any assumptions with your
- *    code in that scenario
+ * - So, whether to handle directories pre or post will actually depend on makes sense for the type of processing that you want to do
+ * - You also can't make any assumptions about the order in which files and directories are visited, and shouldn't make any assumptions
+ *     with your code in that scenario
  *
  */
 public class Main {
--- src/java_files_input_output/_38_walk_filetree/PrintNames.java
+++ src/java_files_input_output/_38_walk_filetree/PrintNames.java
@@ -2,44 +2,80 @@ package java_files_input_output._38_walk_filetree;
 
 import java.io.IOException;
 import java.nio.file.FileVisitResult;
+import java.nio.file.FileVisitor;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
 
 /*
- * SimpleFileVisitor
- *  FileVisitResult.CONTINUE
+ * class : PrintNames extends SimpleFileVisitor<Path>
+
  *
- *  FileVisitResult.SKIP_SIBLINGS
- *      - Traversal should skip all the other entries in the same dir as the file
+ * Overwrite
+ *      visitFile(Path file, BasicFileAttributes attrs)
  *
+ *      - print the absolute path by calling toAbsolutePath on Path obj
+ *      - Instead of returning super , return FileVisitResult.CONTINUE
  *
- *  FileVisitResult.SKIP_SUBTREE
- *      - Used when you want to skip a directory
+ * - Often, it's also a good idea to override postVisitDirectory() as well as the preVisitDirectory, so that you can print the name of
+ *   the directories
  *
- *  FileVisitResult.TERMINATE
- *      - means you want to stop a traversal
- *      - you'd return TERMINATE, when you've found the file that you're searching for, so that the traversal is actually stopped
+ * - So, let's do that as well
+ * - Overwrite
+ *
+ *      preVisitDirectory(Path dir , BasicFileAttributes attrs)
+ *
+ *      - print the absolute path by calling toAbsolutePath on Path obj
+ *      - Instead of returning super , return FileVisitResult.CONTINUE
+ *
+ * - Notice, that we have specified <Path> as the type of the SimpleFileVisitor
+ * - This will also be the type of reference passed to the methods
+ *
+ * - As you can see from visitFile(Path file, BasicFileAttributes attrs)
+ *
+ *      - A Path obj is passed as the first parameter , and it accepts a reference to a file
+ *      - We could be more specific than that because the parameter type has to match the type you specify for the FileVisitor
+ *      - We're also printing the absolute path for the file and then returning FileVisitResult.CONTINUE constant
+ *
+ *      - We can also return FileVisitResult.SKIP_SIBLINGS , which means the traversal should skip all the other entries in the
+ *        same directory as the file
+ *
+ *      - And FileVisitResult.SKIP_SUBTREE can be used when you want to skip a directory
+ *
+ *      - As well as FileVisitResult.TERMINATE, which means you want to stop a traversal
+ *      - For example, you'd return TERMINATE, when you've found the file that you're searching for, so that the traversal is actually stopped
  *
  * - It only makes sense to return FileVisitResult.SKIP_SUBTREE from preVisitDirectory()
- *  - Returning it from other methods is equivalent to continuing
+ * - Returning it from other methods is equivalent to continuing
  *
  *
  * - Also returning FileVisitResult.SKIP_SIBLINGS from the preVisitDirectory() , then the directory itself is also skipped and also the
- *   preVisitDirectory() is never called for that directory
- *      - In this scenario you're basically saying you want to completely ignore the directory and all its descendants and siblings
+ *   postVisitDirectory() is never called for that directory
+ * - In this scenario you're basically saying you want to completely ignore the directory and all its descendants and siblings
  *
  *
- * visitFileFailed()
- *  - If we don't implement this and an error occurs, then an IOException will actually be thrown
- *  - But instead we can notify the user or handle the error and then return continue if you want to actually continue on in the case of an error
- *  - We'll just print out some message here in this case
+ * Lastly, let's overwrite
  *
+ *      visitFileFailed(Path file, IOException exc)
+ *
+ *      - If we don't implement this and an error occurs, then an IOException will actually be thrown
+ *      - But instead we can notify the user or handle the error and then return continue if you want to actually continue on in the case of
+ *        an error
+ *      - We'll just print out some message here in this case
+ *
+ *
+ *
+ * /////
+ * - And nw at this point with the 3 overridden methods, we've got a working FileVisitor
+ *
+ * - So what we need to do is to start the traversal and we'll do using File.walkFileTree()
+ *
+ * - Now let's proceed to the Main class and add the code to do that
  *
  *
  */
 
-public class PrintNames extends SimpleFileVisitor<Path> {
+public class PrintNames implements FileVisitor<Path> {
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
         System.out.println("visiting... "+file.toAbsolutePath());
@@ -56,6 +92,12 @@ public class PrintNames extends SimpleFileVisitor<Path> {
     public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
 
         System.out.println("Error accessing file : "+file.toAbsolutePath() +" "+ exc.getMessage());
+        return FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+        System.out.println("post-visit : "+dir.toAbsolutePath());
         return FileVisitResult.CONTINUE;
     }
 }
