95981966bd0eef6b1a9fd449f03d53778224036f
Alex <alex@stl-horizon.com>
2024-09-10 12:50:29 +0300

    java.nio.file.Files - walkFileTree , copy() , copy entire tree

--- src/java_files_input_output/_39_copy_entire_tree/CopyFiles.java
+++ src/java_files_input_output/_39_copy_entire_tree/CopyFiles.java
@@ -8,14 +8,15 @@ import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
 
 /*
- * Class : CopyFiles that extends SimpleFileVisitor
+ * Class : CopyFiles that extends SimpleFileVisitor/ or implements FileVisitor<Path>
  *
  * Override
- *  visitFailed()
- *      - Print the error
+ *
+ *  visitFileFailed(Path file , IOException exc)
+ *      - Print the absolute path and the error msg
  *      - Return FileVisitResult.CONTINUE
  *
- *  preVisitDirectory()
+ *  preVisitDirectory(Path dir , BasicFileAttributes attrs)
  *      - In order to figure out the path for the copied file, we'll use Path.relativized()
  *          - This method constructs a relative path that resolves to a given path
  *          - e.g. If a path to relativized against is C:\path1 and the given path is C:\path1\path2\path3 , then the relativized path would be
@@ -24,8 +25,8 @@ import java.nio.file.attribute.BasicFileAttributes;
  *      - And that will turn the relative path we got from the relativized() into the full path for the copied file
  *
  * ////
- * - The root of the source path is the FileTree\Dir2
- * - And the root of the destination path is FileTree\Dir4\Dir2 -  (with Dir2) copy folder which at the moment doesn't exist
+ * - The root of the source path is the FileTree\Dir2 - that is where we're starting
+ * - And the root of the destination path is FileTree\Dir4\Dir2Copy folder which at the moment doesn't exist
  *
  * - So, the relative path of the file in both the source and destination roots will be dir3\file1.txt
  *
@@ -42,26 +43,55 @@ import java.nio.file.attribute.BasicFileAttributes;
  *   relative path to both, the source and target roots will be the same
  *
  * - As we may have noticed, we need the source root path and destination root path to figure out the path for the copy
- * - But the preVisitDirectory() and visitFile() are only passed to the directory or file that's being visited
+ * - But the preVisitDirectory() and visitFile() are only passed to reference to the directory or file that's being visited
  *
- * - So we'll have to save the source and destination roots as instance variables in the CopyFiles class  and consequently, add those fields
- *   and a constructor to handle that
+ * - So we'll have to save the source and destination roots as instance variables in the CopyFiles class and consequently, accept the fields
+ *   in the CopyFiles constructor and initialize them respectively
+ *
  *
  *
  * ////////
- *  - preVisitDirectory()
- *      - we'll use relativized and resolve methods to figure out the path for the copied file
- *      - Once we get the resolved destination path, we can call the copy() to do the actual copy
- *      - In case of an error, skip the enitre directory and it's contents via the FileVisitResult.SKIP_SUBTREE constant
+ * - Overwrite
  *
- *  - visitFile()
+ *      preVisitDirectory(Path dir , BasicFileAttribute attrs)
+ *
+ *      - we're getting the relative path to the source root using the relativized()
+ *      - The we're calling resolve() to get the path for the copied file
+ *      - Once we get the resolved destination path, we call Files.copy(dir, copyDir) to do the actual copy
+ *              Files.copy(dir , copyDir);
+ *
+ *              - is equivalent to "Files.copy("FileTree\Dir2\Dir3" , "FileTree\Dir4\Dir2Copy\Dir3")"
+ *
+ *      - In case of an error, skip the entire directory and it's contents by returning
+ *              FileVisitResult.SKIP_SUBTREE;
+ *              - and this means stop processing processing entries for that directory
+ *
+ *      - Otherwise if all goes well, return
+ *              FileVisitResult.CONTINUE;
+ *
+ * - Next, overwrite
+ *
+ *      visitFile(Path file , BasicFileAttribute attrs)
+ *
  *      - we'll use relativized and resolve methods to figure out the path for the copied file
+ *
+ *              Path relativizedPath = sourceRoot.relativize(file);
+ *
  *      - Once we get the resolved destination path, we can call the copy() to do the actual copy
+ *
+ *              Path copyPath = targetRoot.resolve(relativizedPath);
+ *
  *      - In case of an exception, we'll just print the error message
+ *              System.out.println(exc.getMessage());
+ *
+ *      - If all works well, return
+ *              FileVisitResult.CONTINUE;
+ *
+ *      - Quite similar to preVisitDirectory()
  *
  *
  * ///
- *  - Kick off the copy
+ *  - Kick off the copy the same way we did when we printed the file tree
  */
 
 public class CopyFiles extends SimpleFileVisitor<Path> {
@@ -77,14 +107,14 @@ public class CopyFiles extends SimpleFileVisitor<Path> {
     @Override
     public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
         System.out.println("preVisitDirectory called ...");
-        Path relativizedPath = sourceRoot.relativize(dir);        // "FileTree\Dir2".relativize("FileTree\Dir2\Dir3\file1.txt");
-        System.out.println("RelativizedPath = "+relativizedPath); // relativizedPath = "Dir3\file1.txt"
+        Path relativizedPath = sourceRoot.relativize(dir);        // "FileTree\Dir2".relativize(FileTree\Dir2\Dir3);
+        System.out.println("RelativizedPath = "+relativizedPath); // relativizedPath = Dir3
 
-        Path copyDir = targetRoot.resolve(relativizedPath); // "FileTree\Dir4\Dir2Copy".resolve("Dir3\file1.txt");
-        System.out.println("Resolved path for copy = "+copyDir); // copyDir = "FileTree\Dir4\Dir2Copy\Dir3\file1.txt"
+        Path copyDir = targetRoot.resolve(relativizedPath); // "FileTree\Dir4\Dir2Copy".resolve("Dir3");
+        System.out.println("Resolved path for copy = "+copyDir); // Resolved path for copy = "FileTree\Dir4\Dir2Copy\Dir3"
 
         try{
-            Files.copy(dir , copyDir);                      // Files.copy("FileTree\Dir2\Dir3\file1.txt" , "FileTree\Dir4\Dir2Copy\Dir3\file1.txt");
+            Files.copy(dir , copyDir);                      // Files.copy("FileTree\Dir2\Dir3" , "FileTree\Dir4\Dir2Copy\Dir3");
         }catch (IOException exc){
             System.out.println(exc.getMessage());
             return FileVisitResult.SKIP_SUBTREE;
@@ -97,13 +127,13 @@ public class CopyFiles extends SimpleFileVisitor<Path> {
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
         System.out.println("visitFile called ...");
         Path relativizedPath = sourceRoot.relativize(file);        // "FileTree\Dir2".relativize("FileTree\Dir2\Dir3\file1.txt");
-        System.out.println("RelativizedPath = "+relativizedPath); // relativizedPath = "Dir3\file1.txt"
+        System.out.println("RelativizedPath = "+relativizedPath); // RelativizedPath = "Dir3\file1.txt"
 
-        Path copyDir = targetRoot.resolve(relativizedPath); // "FileTree\Dir4\Dir2Copy".resolve("Dir3\file1.txt");
-        System.out.println("Resolved path for copy = "+copyDir); // copyDir = "FileTree\Dir4\Dir2Copy\Dir3\file1.txt"
+        Path copyPath = targetRoot.resolve(relativizedPath); // "FileTree\Dir4\Dir2Copy".resolve("Dir3\file1.txt");
+        System.out.println("Resolved path for copy = "+copyPath); // Resolved path for copy = "FileTree\Dir4\Dir2Copy\Dir3\file1.txt"
 
         try{
-            Files.copy(file , copyDir);                      // Files.copy("FileTree\Dir2\Dir3\file1.txt" , "FileTree\Dir4\Dir2Copy\Dir3\file1.txt");
+            Files.copy(file , copyPath);                      // Files.copy("FileTree\Dir2\Dir3\file1.txt" , "FileTree\Dir4\Dir2Copy\Dir3\file1.txt");
         }catch (IOException exc){
             System.out.println(exc.getMessage());
         }
--- src/java_files_input_output/_39_copy_entire_tree/Main.java
+++ src/java_files_input_output/_39_copy_entire_tree/Main.java
@@ -9,39 +9,55 @@ import java.nio.file.Path;
 /*
  * Copy Entire Free
  *
- * - Let's copy everything in dir2 folder to a dir2 directory that is within the dir4 directory
+ * - Let's copy everything in FileTree\dir2 folder to a dir2 directory that is within the dir4 directory
  *
- * - To do this, we'll override the
- *      - visitFile()
- *      - preVisitDirectory()
- *      - postVisitDirectory()
+ * - To do this, we'll override the visitFile() , preVisitDirectory() and the postVisitDirectory()
  *
  * - The visitFile() , will be the same as it is for the PrintNames example
  *
  * - Create a new class for this
  *      - CopyFiles that extends SimpleFileVisitor class
  *
+ *      - Check if it can also implement FileVisitor
+ *
  *
  * /////////
- *  - Kickstart things off
- *      - Add
- *          sourcePath
- *          copyPath
+ * - Kickstart things off
+ *
+ * - Create Path sourcePath obj
+ *
+ *      Path sourcePath = FileSystems.getDefault().getPath("FileTree" +File.separator+ "Dir2");
+ *
+ * - Create Path copyPath obj
+ *
+        Path copyPath = FileSystems.getDefault().getPath("FileTree" + File.separator + "Dir4" +File.separator+ "Dir2Copy");
+ *
  *
  *  - Use a try block
+ *
  *      - call Files.walkFileTree()
- *          - pass sourcePath as the first arg
- *          - pass a new instance of the CopyFiles(sourcePath,dirPath) as the 2nd parameter
+ *
+ *          Files.walkFileTree(sourcePath , new CopyFiles(sourcePath , copyPath));
+ *
+ *          - pass sourcePath as the 1st parameter
+ *          - pass a new instance of the CopyFiles(sourcePath,dirPath) as the 2nd parameter which we all know that it implements FileVisitor
  *
  *  - catch any IOException
+ *      - print the error message
  *
  *
- * //
- *  - We have been able to copy the entire tree now which is pretty cool
- *      - In a real world application, we should consider checking destination folders and probably ask user whether they want to overwrite existing
- *        contents in the destination folder if it already exist
+ * ////
+ * - Running this works well and we're able to copy all the contents of FileTree\Dir2 to FileTree\Dir2\Dir2Copy
+ * - We have been able to copy the entire tree now which is pretty cool
  *
- *  - In addition, you can pass more parameters to the copy() to specify what should happen if the destination path already exists
+ *
+ * //////
+ *
+ * - In a real world application, we should consider checking destination files/folders whether they exist and probably ask/prompt users whether they
+ *   want to overwrite existing contents in the destination folder if that happens
+ *
+ * - In addition, you can pass more parameters to the copy() to specify what should happen if the destination path already exists
+ * - You have to consider all these scenarios in a real world app
  *
  */
 public class Main {
