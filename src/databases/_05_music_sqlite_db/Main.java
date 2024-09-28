d73d549868c6022085ac6de8f07350f30ab5f45e
Alex Mwangi <alex@stl-horizon.com>
2024-09-28 12:50:13 +0300

    Executing SQL in DB Browser

--- src/databases/_05_music_sqlite_db/Main.java
+++ src/databases/_05_music_sqlite_db/Main.java
@@ -143,10 +143,10 @@ import java.util.Scanner;
  *
  * In this scenario, the callers of the method will still have to handle a null return value , however they see it
  *  fit
- * In other words the process that's calling the queryArtists() has to actually check and see whether what we're
+ * In other words the process that's calling the queryArtist() has to actually check and see whether what we're
  *  getting back in NULL or not
  *
- * //// Calling queryArtists() ///////
+ * //// Calling queryArtist() ///////
  *
  *  List<Artist> artists = datasource.queryArtist();
  *
@@ -191,6 +191,239 @@ import java.util.Scanner;
  *  the table column indices
  * We'll start adding all the Column indices constants for all the tables
  *
+ *
+ *
+ * ///////////////////////////////
+ * Executing SQL in DB Browser
+ * ///////////////////////////////
+ *
+ * We'll start by adding column index CONSTANTS for all the tables
+ * Unlike everything else in Java, column indices are 1-based
+ *  - The first col is at position 1
+ *  - The second col is at position 2
+ * If we use a zero, we'll actually get an exception
+ *
+ * Let's add the column indices CONSTANTS for all our tables as below
+ *
+ * Artist Table
+ *      public static final int INDEX_ARTIST_ID = 1;
+        public static final int INDEX_ARTIST_NAME = 2;
+ *
+ *
+ * Albums Table
+ *      public static final int INDEX_ALBUM_ID = 1;
+        public static final int INDEX_ALBUM_NAME = 2;
+        public static final int INDEX_ALBUM_ARTIST = 3;
+ *
+ * Songs Table -
+ *      public static final int INDEX_SONG_ID = 1;
+        public static final int INDEX_SONG_TRACK = 2;
+        public static final int INDEX_SONG_TITLE = 3;
+        public static final int INDEX_SONG_ALBUM = 4;
+ *
+ *
+ * Let's now update the queryArtist() to use the column indices inside the while loop as below, becaose at the moment
+ *  we were using column names
+ *
+ *      while (results.next()){
+                Artist artist = new Artist();
+                artist.setId(results.getInt(COLUMN_ARTIST_ID));
+                artist.setName(results.getString(COLUMN_ARTIST_NAME));
+                artists.add(artist);
+            }
+ *
+ * So let's change that to
+ *
+ *      while (results.next()){
+                Artist artist = new Artist();
+                artist.setId(results.getInt(INDEX_ARTIST_ID));
+                artist.setName(results.getString(INDEX_ARTIST_NAME));
+                artists.add(artist);
+            }
+ *
+ * Run the program and make sure everything is still working
+ *
+ * ///
+ * It's more efficient to use the column index because the getter methods will know exactly where to go and get the
+ *  value in the result set
+ * When we use the column names, the method has to match the column name against the columns in the result set
+ * That doesn't matter when we're looping through a small set of records, but if we were to loop through thousands
+ *  of records, it can as well make a difference
+ * The queryArtist() retrieves all the records from the artists table
+ * While this is useful, we may need a way to retrieve specific records
+ * When working with a real-world application, we don't usually have to accommodate every possible query that can
+ *  be made against every table
+ * We usually look at the UI for the application, whether that is a web page, a mobile screen or a desktop window
+ *  and we figure out what queries we need to perform to populate each web page, window,screen etc with data
+ * And those are the queries we'd want to accommodate in our data source
+ *
+ * ///
+ * So, let's think about what queries an application using the music database might want to perform and also the SQL
+ *  statements we'd use to get the data
+ * Let's assume our application allows the user to do the following
+ *      - View a list of artists
+ *      - View all albums by a particular artist
+ *      - View all songs on a particular album
+ *      - Find out which artist recorded a particular album
+ *      - Find out which album the song is on
+ *      - Find out which artist recorded a particular song along with the album the song is on and the track no
+ *
+ * Amongst others, but we'll stick to this set for this application
+ * We've already coded queryArtist() that retrieves the list of artists
+ *
+ * //////////
+ * Let's now write a method that retrieves all the albums for a particular artist, in other words viewing all the
+ *  albums by a particular artist
+ * Unfortunately, there isn't an elegant way to build query strings when using JDBC
+ * It would be nice to be able to create a query string by chaining methods together like
+ *  select()
+ *      .join()
+ *      .orderBY()
+ * and there may well be 3rd party libraries that give us the ability to do just that, but the base API doesn't
+ *  unfortunately
+ * To perform the query we want, we have to build a query string and pass it to the statement.executeQuery() or
+ *  execute
+ * But first, let's modify the queryArtist() to accept a parameter that indicates how we want the artists to be
+ *  sorted
+ * We'll need String constants in the Datasource class that callers can use to specify the sort order
+ * So let's add them first
+ *
+ *      public static final int ORDER_BY_NONE = 1;
+        public static final int ORDER_BY_ASC = 2;
+        public static final int ORDER_BY_DESC = 3;
+ *
+ * Then in the queryArtist() , instead of passing "SELECT * FROM artists" as we're doing currently, we're going to
+ *  build a query string using a StringBuilder
+ *
+ * We'll start by adding a query argument to our method: queryArtist(int sortOrder)
+ * Then create a StingBuilder class
+ *
+ *      StringBuilder sb = new StringBuilder("SELECT * FROM ");
+ *
+ * And then append the Table name as follows
+ *
+        sb.append(TABLE_ARTISTS);
+ *
+ * Then check the sortOrder value passed to queryArtist
+ *
+ * If the caller passes Datasource.ORDER_BY_NONE
+ *  - we ignore adding the orderBy clause completely to the SQL statement
+ *
+ * Otherwise, if the value is none other than Datasource.ORDER_BY_NONE, Then we append the ORDER BY clause as below
+ *
+ *      append orderBy name with COLLATE NOCASE clause
+ *
+ *      - used COLLATE NOCASE to do case-insensitive order
+ *
+ * Next we're then we're checking the descending and ascending depending on the parameter that's actually passed
+ *
+ * Check if sortOrder value passed matches ORDER_BY_DESC constant
+ * If so,
+ *      append "DESC" to the String builder
+ * Otherwise,
+ *      append "ASC" to the String builder
+ *
+ *      if (sortOrder != ORDER_BY_NONE){
+            sb.append(" ORDER BY ");
+            sb.append(COLUMN_ARTIST_NAME);
+            sb.append(" COLLATE NOCASE ");
+            if (sortOrder == ORDER_BY_DESC)
+                sb.append("DESC");
+            else
+                sb.append("ASC");
+        }
+ *
+ * We could have changed the sort order to use an enum instead, so optionally we could do that and use it in some
+ *  instances that could make more sense because we'd be validating the enum as a parameter
+ * But for now the way we've defined the queryArtist(int sortOrder) and the int that has been passed to it,
+ * it's just an integer and so it's possible to pass an invalid value that wouldn't be used
+ *
+ * Now that we have constructed the SQL statement, we then need to pass our String builder query string to the
+ *  executeQuery(sb.toString())
+ *
+ * Also note that the if condition we're checking ORDER_BY_DESC and not ORDER_BY_ASC and we're doing that because
+ *  if a caller passed a value that doesn't match one of the Sort CONSTANTS
+ * In that case, our method will end up returning the artist sorted in ASC order, which is a reasonable default
+ * We could have checked that the caller had passed a valid value and throw an exception if they hadn't or they used
+ *  an enum instead, but having a default behavior like this is nicer
+ *
+ * //////
+ * Let's modify the call of the queryArtist() and pass one of the SORT Constants
+ *
+ *      List<Artist> artists = datasource.queryArtist(Datasource.ORDER_BY_ASC);
+ *
+ *      - and pass order by ASC by default
+ * And now if we run this, we get a list of artists sorted by artist name printed to the console
+ * Check also the others, just to make sure they're working
+ *      List<Artist> artists = datasource.queryArtist(Datasource.ORDER_BY_DESC);
+ *
+ *      - and the order by descending is working as well
+ *
+ *      List<Artist> artists = datasource.queryArtist(Datasource.ORDER_BY_NONE);
+ *
+ *      - and this implies no sorting, which is also working
+ *
+ * And also if we pass a number that we know is not represented with a CONSTANT
+ *
+ *      List<Artist> artists = datasource.queryArtist(5);
+ *
+ *      - we get the artists sorted by ascending order by default
+ *
+ *
+ *
+ *
+ * ///////////////
+ * Write a method that will retrieve all the albums by a particular artist
+ * But before we do let's try the SQL statement in the DB Browser
+ * It's always good practice to test the SQL we want to use before coding it
+ * We didn't do for the SQL statement that retrieves all the artists because it was simple , but when we get into
+ *  complex queries, it's always good to run the queries against the database before coding them
+ *
+ * Suppose we want to find out all the albums by Iron Maiden artist from the albums table by running the following
+ *  SQL
+ * We can also execute SQL here in the DB Browser interface by clicking on Execute SQL tab and write below SQL
+ *  statement
+ *
+ *      SELECT * FROM albums WHERE artist = 8;
+ *
+ *      - and if we run this, we got 3 entries returned
+ * So, now that we know what to expect from the query that will use a join, let's run the query we use against the
+ *  database
+ * Open a new tab from the DB Browser and type in the command below
+ *
+ *      SELECT albums.name FROM albums
+ *      INNER JOIN artists ON albums.artist = artists._id
+ *      WHERE artists.name  = 'Iron Maiden'
+ *      ORDER BY albums.name COLLATE NOCASE ASC;
+ *
+ *      - and we get the same 3 entries returned similar to the ones returned above
+ *
+ * For good measure, let's try another artist : Pink Floyd , has an id of 130
+ *
+ *      SELECT * FROM albums WHERE artist = 130;
+ *
+ *      - and we get a lot more albums returned, 12 in total
+ *
+ * And now we need to update below to Pink Floyd
+ *
+ *      SELECT albums.name FROM albums
+ *      INNER JOIN artists ON albums.artist = artists._id
+ *      WHERE artists.name  = 'Pink Floyd'
+ *      ORDER BY albums.name COLLATE NOCASE ASC;
+ *
+ *      - and sure enough we get the same results as above, 12 albums for Pink Floyd
+ *
+ * Let's also try another artist whose got only 1 album named Tapestry : Carole King , with
+ *
+ *      SELECT albums.name FROM albums
+ *      INNER JOIN artists ON albums.artist = artists._id
+ *      WHERE artists.name  = 'Carole King'
+ *      ORDER BY albums.name COLLATE NOCASE ASC;
+ *
+ *      - and now if we run this, we get that 1 album Tapestry returned
+ *
+ * At this point we've done 3 test case, with 3 test queries and now we're good to go
+ * We'll start writing a method for querying albums for artists in Java using JDBC to execute these queries
  *
  */
 
