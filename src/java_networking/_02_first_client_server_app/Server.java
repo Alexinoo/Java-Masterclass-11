3b894962c4e0385b2faaa9f60038a8ddb3a67c1c
Alex <alex@stl-horizon.com>
2024-10-04 16:20:42 +0300

    First Client and Server Apps

---
+++ src/java_networking/_02_first_client_server_app/Server.java
@@ -0,0 +1,35 @@
+package java_networking._02_first_client_server_app;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/*
+ * First Client and Server Apps
+ */
+public class Server {
+
+    public static void main(String[] args) {
+        try(ServerSocket serverSocket = new ServerSocket(5000)){
+
+            Socket socket = serverSocket.accept();
+            System.out.println("Client connected");
+
+            BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter output = new PrintWriter(socket.getOutputStream() , true);
+
+            while (true){
+                String echoString = input.readLine();
+                if (echoString.equals("exit"))
+                    break;
+
+                output.println("Echo from server: "+echoString);
+            }
+        }catch (IOException ioe){
+            System.out.println("Server Exception "+ioe.getMessage());
+        }
+    }
+}
