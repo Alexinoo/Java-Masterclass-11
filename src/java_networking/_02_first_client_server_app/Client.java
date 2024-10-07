72bcb36427e6de9009dca9c8b9cab3d172ceb409
Alex <alex@stl-horizon.com>
2024-10-07 12:55:20 +0300

    First Client and Server Apps

--- src/java_networking/_02_first_client_server_app/Client.java
+++ src/java_networking/_02_first_client_server_app/Client.java
@@ -7,13 +7,96 @@ import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Scanner;
 
+/*
+ * Client App
+ *
+ * Steps
+ *
+ * - We're going to create a Socket and not a ServerSocket using try-with-resources statement
+ *      - The address is going to be localhost and port no 5000
+ *      - The difference here is that for our Client app, we need to specify 2 parameters
+ *          - The address of the host we want to connect to
+ *          - The port no
+ *      - We've used "localhost" here because we want to connect to a server running on the same host, which in this case is my computer
+ *      - We can also try passing the ip address "127.0.0.1" if "localhost" doesn't work
+ *      - We have to specify the same port no for our client app because that's the port no that the server is listening on
+ *
+ *          Socket socket = new Socket("localhost",5000)
+ *
+ *      - How do we know which port no to use ?
+ *          - If we're writing both the client and the server , you'll know
+ *          - If we're writing a client that wants to connect to a server that you didn't write, the documentation for the API you're using
+ *             or the server application you're trying to connect to should tell you the port no that your code has to use
+ *
+ * - Create the Input and Output streams in the same way we did for the server
+ *
+ *           BufferedReader echoes = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+             PrintWriter stringToEcho = new PrintWriter(socket.getOutputStream() , true);
+
+             - Quite similar to our PrintWriter, flushes the output automatically, instead of us having to manually do that every time we send
+                the data
+
+
+ * - We could hard code Strings to send to the server, but let's actually ask the user input instead
+ *      - We'll use a Scanner class to read the input from the console and once we have the String, we're goint to write it to the socket using
+ *          PrintWriter
+ *      - We'll also have to add a check here for the "exit" String
+ *      - When the user types in exit, that means that they want to quit and therefor we'll exit from the loop
+ *
+ *          Scanner scanner = new Scanner(System.in);
+ *          String echoString;
+            String response;
+
+        - We'll use a do-while loop,
+
+            do {
+                System.out.println("Enter string to be echoed: ");
+                echoString = scanner.nextLine();
+
+                stringToEcho.println(echoString);
+
+                if (!echoString.equals("exit")) {
+                    response = echoes.readLine();
+                    System.out.println(response);
+                }
+
+             }while (!echoString.equals("exit"));
+
+         - We're using a do while loop here because we want the loop to execute at least once, because we don't have any idea how many
+            times it'll execute
+         - First, we're asking the user for a String, and then we're sending the string to the server using the PrintWriter obj
+
+                System.out.println("Enter string to be echoed: ");
+                echoString = scanner.nextLine();
+                stringToEcho.println(echoString);
+
+         - Then we're just checking whether the String entered equals exit, and if it does, the loop's going to terminate
+         - And if the String is not equal to "exit" then we'll continue reading the data coming back from the server using BufferedReader
+            & then we're outputting what we got back from the server
+
+              if (!echoString.equals("exit")) {
+                    response = echoes.readLine();
+                    System.out.println(response);
+                }
+
+         - The while loop continues until we receive an exit
+ *
+ * - Catch IOException
+ *      - Print error message
+ *
+ *
+ *
+ * ///////
+ *
+ * We've got now our server and our client applications, it's now time to run both applications
+ */
+
 public class Client {
 
     public static void main(String[] args) {
         try(Socket socket = new Socket("localhost",5000)){
 
             BufferedReader echoes = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-
             PrintWriter stringToEcho = new PrintWriter(socket.getOutputStream() , true);
 
             Scanner scanner = new Scanner(System.in);
@@ -24,9 +107,12 @@ public class Client {
                 System.out.println("Enter string to be echoed: ");
                 echoString = scanner.nextLine();
 
+                // Send the String to the server
                 stringToEcho.println(echoString);
 
                 if (!echoString.equals("exit")) {
+
+                    // receive the string from the server
                     response = echoes.readLine();
                     System.out.println(response);
                 }
--- src/java_networking/_02_first_client_server_app/Server.java
+++ src/java_networking/_02_first_client_server_app/Server.java
@@ -9,6 +9,117 @@ import java.net.Socket;
 
 /*
  * First Client and Server Apps
+ *
+ * We'll write 2 applications to demonstrate how to do network coding
+ * One application is going to be the server and the other will be the client
+ *
+ * We'll start to work on the server application and it's purpose is just going to echo back to the client whatever text the client
+ *  sends it
+ *
+ * Steps
+ *  - Create a ServerSocket using try-with-resources statement
+ *
+ *      ServerSocket serverSocket = new ServerSocket(5000);
+ *
+ *      - The port no that we've allocated here, that can be an integer between 0 and 65535 inclusive
+ *      - Some port numbers are already reserved or may already be in use by other applications
+ *      - The way to find out is to assign a port number and see if the socket instance is successfully created
+ *      - Depending on the application, we can do some research on the internet to find out what specific applications or which port numbers
+ *         specific applications use, but for now we'll just work with 5000
+ *
+ *  - We want the Server to listen for clients on the Port that we've assigned
+ *      - To do that we call serverSocket.accept() and that returns a Socket Instance/obj
+ *
+            Socket socket = serverSocket.accept();
+            System.out.println("Client Connected");
+
+            - And notice that it is a Socket instance and not a ServerSocket
+            - This will be the socket used to communicate with the client
+            - Keep in mind that every client that connects to a server will do so using the same port but through it's own socket
+
+        - The accept(), is going to block until a client connects to the server
+        - To show that, we're going to add a println statement after the call to accept
+
+        - Note that if we run this, we don't see the message, "Client Connected" and that's because the application in this case the Server,
+           is waiting for a client to connect to it
+        - We obviously, haven't got a client bit of code written yet, and we'll have to disconnect manually
+
+ *  - When a Client connects, the Server will use input and output streams to send data to the client and receive data from the client
+ *      - The socket.getInputStream() and socket.getOutputStream() methods will return the input and output streams associated with the
+ *         ServerSocket instance
+ *
+ *          BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter output = new PrintWriter(socket.getOutputStream(),true);
+
+            - A common practice is to wrap the input stream with a BufferedReader and to wrap the output stream with the PrintWriter
+            - The 2nd parameter to the PrintWriter constructor specifies whether you want to automatically flush the output stream the
+               PrintWriter is using
+            - If you don't specify true, then you'll have to call the flush() after every write to the stream to make sure data is sent
+
+ *
+ *  - Now it's time to read data from the client
+ *      - At this point, we can use BufferedReader as we would - there's nothing special about a BufferedReader attached to a socket,
+ *      - You want the server to remain alive until the client doesn't need it anymore, and so this is one time when you'll intentionally
+ *         use an infinite loop
+ *
+ *          while(true){
+ *
+ *              String echoString = input.readLine();
+ *              if(echoString.equals("exit"))
+ *                  break;
+ *
+ *              output.println("Echo from server: "+ echoString);
+ *
+ *          }
+ *
+ *          - Read from the BufferedReader readLine() which reads from the client
+ *          - When the server receives the String "exit" from the client, it's going to check that,terminate and exit from the infinite loop
+ *          - But if it's not "exit", then we're going to echo the string back to the client with the prefix "Echo From Server"
+ *
+ *      - Then we write the String to the socket using PrintWriter println()
+ *
+ *  - Catch an IOException
+ *      - Print "Server exception" and the error message
+ *      - Print StackTrace
+ *
+ *
+ *  That's all there is to writing the server, though we'll make some improvements to this server later
+ *
+ * /////////////////
+ *
+ * Now let's write the Client application
+ *  - Create Client.java file
+ *
+ *
+ * ////// Running the app
+ *
+ * Run the Server.java first
+ *
+ * Run the Client.java second
+ *
+ * IF we look at the Server.java console, we now get "Client connected" to the console
+ * At this point, the client is connected to the server and so the accept() in the Server.Main.java is no longer blocked and the code
+ *  continues executing
+ *
+ * So we're now executing while loop right now, and it's basically sitting there waiting on the readLine(), waiting to receive some input
+ *  from the client
+ *
+ * If we type, "Hello From Server", we get an "Echo From Server: Hello, Server" response back
+ * And we'll continue getting the response that we write to the server until we type "exit"
+ *
+ *
+ * If we type in "exit", this disconnects both Server.Main.java and Client.Main.java sessions
+ *
+ * At this point we've now written my first client and also my first server
+ *
+ * But the Server we've written isn't very useful because it can only connect to 1 client
+ *
+ * We'll start modifying it so that we can accept connections for multiple clients
+ *
+ *
+ *
+ *
+ *
  */
 public class Server {
 
